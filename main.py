8f4339570acf2447efa6fefd9637ac1dd3901713
Harleen <harleen365.be22@chitkara.edu.in>
2025-03-15 22:12:40 +0530

    adding 3 images

--- main.py
+++ main.py
@@ -8,14 +8,40 @@ class Face_Recognition_System:
         self.root.geometry("1530x790+0+0")
         self.root.title("face Recognition System")
         
-
+        #first image
         img=Image.open()
         #open k ander images ka path aaega:timestamp 26:57
         img=img.resize((500,130),Image.ANTIALIAS)
         self.photoimg=ImageTk.PhotoImage(img)
 
+
+
         f_lbl=Label(self.root,image=self.photoimg)
         f_lbl.place(x=0,y=0,width=500,height=130)
+
+        #2nd image
+        img1=Image.open()
+        #open k ander images ka path aaega:timestamp 26:57
+        img1=img1.resize((500,130),Image.ANTIALIAS)
+        self.photoimg=ImageTk.PhotoImage(img1)
+
+
+
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=500,height=130)
+
+        #3rd image
+        img2=Image.open()
+        #open k ander images ka path aaega:timestamp 26:57
+        img2=img2.resize((500,130),Image.ANTIALIAS)
+        self.photoimg=ImageTk.PhotoImage(img2)
+
+
+
+        f_lbl=Label(self.root,image=self.photoimg)
+        f_lbl.place(x=0,y=0,width=500,height=130)
+
+
 if __name__ == "__main__":
     root=Tk()
     obj= Face_Recognition_System(root)
